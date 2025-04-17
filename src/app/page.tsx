891442d4305f66164d566bc030cd1a59c30c03f7
Jong Woo Park <jwmtp2@gmail.com>
2025-04-17 17:16:32 +0900

    feat: 결제 버튼 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,13 @@
 'use client';
 import styles from "./page.module.css";
 import TossPayments from "@/component/tosspayments";
-import {TossPaymentsWidgets} from "@tosspayments/tosspayments-sdk";
-import {useState} from "react";
-
 
 export default function Home() {
-    const [widget, setWidget] = useState<TossPaymentsWidgets>();
-
-    const handleInit = (widget: TossPaymentsWidgets) => {
-        setWidget(widget)
-    }
-
-    const handlePayClick = async () => {
-        await widget?.requestPayment({
-            orderId: '',
-            orderName: '테스트'
-        });
-    }
 
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <button onClick={handlePayClick}>결제</button>
-                <TossPayments amount={10000} onInit={handleInit}/>
+                <TossPayments amount={10000}/>
             </main>
         </div>
     );
